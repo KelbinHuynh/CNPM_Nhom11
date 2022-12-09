cefad7d21bb6f1bc374f844c63336db592bcdcf3
Huỳnh Anh Vũ <93385678+KelbinHuynh@users.noreply.github.com>
2022-12-09 23:07:25 +0700

    Delete WebCNPM directory

--- WebCNPM/src/project/dao/IAdminDao.java
+++
@@ -1,7 +0,0 @@
-package project.dao;
-
-import project.model.Admin;
-
-public interface IAdminDao {
-	Admin findaccount(String id);
-}
--- WebCNPM/src/project/service/IAccountService.java
+++
@@ -1,9 +0,0 @@
-package project.service;
-
-import project.model.Account;
-
-public interface IAccountService {
-	Account loginAccount(String username, String password);
-	Account findbyUsername(String username);
-	boolean checkExistUsername(String username);
-}
